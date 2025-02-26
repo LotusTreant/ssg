57ab559f0669d025138784f72a334c617ae22b7c
LotusTreant <mrc.2040@gmail.com>
2025-02-26 11:10:12 -0500

    add github hosting

--- src/main.py
+++ src/main.py
@@ -1,20 +1,25 @@
 import os
 import shutil
+import sys
 from copystatic import copy_files
 from gen_content import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
     print("Copying static files to public directory...")
     copy_files(dir_path_static, dir_path_public)
     print("Generating page...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
-    
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
+
 main()
